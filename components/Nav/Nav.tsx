3d6cc5310424e9299894f643d0e3b4c9c8b6be08
jeffreyshum <89279966+jeffreyshum@users.noreply.github.com>
2022-04-21 00:33:49 -0400

    Added Navigation to Saved Podcasts

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,13 +1,23 @@
+import Link from "next/link"
 import { FC } from "react"
 import styles from "./Nav.module.css"
 
 const Nav: FC = () => {
 	return (
-		<nav
-			className={styles.nav}
-			onClick={() => (window.location.href = "/")}>
-			<img className={styles.logo} src="/images/logo.svg" alt="Logo" />
-			<h1 className={styles.title}>Cast Away</h1>
+		<nav className={styles.nav}>
+			<div
+				className={styles.logoContainer}
+				onClick={() => (window.location.href = "/")}>
+				<img
+					className={styles.logo}
+					src="/images/logo.svg"
+					alt="Logo"
+				/>
+				<h1 className={styles.title}>Cast Away</h1>
+			</div>
+			<Link href="/saved" passHref={true}>
+				<a className={styles.link}>Saved</a>
+			</Link>
 		</nav>
 	)
 }
